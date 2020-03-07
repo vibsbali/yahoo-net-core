90165193aa566751121c8d1f41175711d726f7f3
Vaibhav Bali <vbali@internode.on.net>
2020-03-07 20:05:26 +1100

    Added crude task methodology

--- YahooFin/Program.cs
+++ YahooFin/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
@@ -9,46 +7,13 @@ namespace YahooFin
 {
    class Program
    {
-      public static async Task DownloadData(string symbol, long startDate, long endDate, string crumb, HttpClient httpClient)
+      public static async Task<string> DownloadData(string symbol, long startDate, long endDate, string crumb, HttpClient httpClient)
       {
          var url = $"https://query1.finance.yahoo.com/v7/finance/download/{symbol}?period1={startDate}&period2={endDate}&interval=1d&events=history&crumb={crumb}";
          var response = await httpClient.GetAsync(url);
 
          string responseBody = await response.Content.ReadAsStringAsync(); ;
-         Console.WriteLine(responseBody);
-         //System.out.println(url);
-
-         //request.addHeader("User-Agent", "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.13) Gecko/20101206 Ubuntu/10.10 (maverick) Firefox/3.6.13");
-         //try
-         //{
-         //   HttpResponse response = client.execute(request, context);
-         //   System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
-         //   HttpEntity entity = response.getEntity();
-
-         //   String reasonPhrase = response.getStatusLine().getReasonPhrase();
-         //   int statusCode = response.getStatusLine().getStatusCode();
-
-         //   System.out.println(String.format("statusCode: %d", statusCode));
-         //   System.out.println(String.format("reasonPhrase: %s", reasonPhrase));
-
-         //   if (entity != null)
-         //   {
-         //      BufferedInputStream bis = new BufferedInputStream(entity.getContent());
-         //      BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(new File(filename)));
-         //      int inByte;
-         //      while ((inByte = bis.read()) != -1)
-         //         bos.write(inByte);
-         //      bis.close();
-         //      bos.close();
-         //   }
-         //   HttpClientUtils.closeQuietly(response);
-
-         //}
-         //catch (Exception ex)
-         //{
-         //   System.out.println("Exception");
-         //   System.out.println(ex);
-         //}
+         return responseBody;
       }
 
       public static void Main(string[] args)
@@ -56,6 +21,10 @@ namespace YahooFin
          var httpClient = new HttpClient();
          httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36");
          var jsonObject = new JObject();
+
+         
+         
+
 
          var task = Task.Run(async () =>
          {
@@ -76,7 +45,25 @@ namespace YahooFin
 
          task.Wait();
          var taskTwo = Task.Run(() => DownloadData("LNK.AX", 1551683555, 1583305955, jsonObject["crumb"].ToString(), httpClient));
-         taskTwo.Wait();
+         var taskThree = Task.Run(() => DownloadData("CBA.AX", 1551683555, 1583305955, jsonObject["crumb"].ToString(), httpClient));
+         var taskFour = Task.Run(() => DownloadData("NHC.AX", 1551683555, 1583305955, jsonObject["crumb"].ToString(), httpClient));
+         var taskFive = Task.Run(() => DownloadData("BHP.AX", 1551683555, 1583305955, jsonObject["crumb"].ToString(), httpClient));
+
+         var finalTask = Task.Run(async () =>
+         {
+            await taskTwo;
+            await taskThree;
+            await taskFour;
+            await taskFive;
+         });
+
+         finalTask.Wait();
+         Console.WriteLine("Result of Task Two");
+         Console.WriteLine(taskTwo.Result);
+         Console.WriteLine("Result of Task Three");
+         Console.WriteLine(taskThree.Result);
+         Console.WriteLine("Result of Task Four");
+         Console.WriteLine(taskFour.Result);
       }
 
 
